7f07e51ac9de0578df6f0898a71282eae2891d8d
Nicolazz07 <nicolas.zapata122610@gmail.com>
2026-08-28 22:20:31 -0500

    Implementar lógica del carrito con localStorage y contador dinámico

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,6 +1,7 @@
 // Esperar a que la página cargue por completo
 document.addEventListener('DOMContentLoaded', () => {
     cargarCatalogo();
+    actualizarContadorCarrito(); 
 });
 
 // Función principal para obtener productos de la API
@@ -42,7 +43,7 @@ async function cargarCatalogo() {
                             ${producto.descripcion_corta}
                         </p>
                         <p class="card-precio">${precioFormat}</p>
-                        <button class="btn-agregar" onclick="agregarAlCarrito(${producto.id_producto})">
+                        <button class="btn-agregar" onclick="agregarAlCarrito(${producto.id_producto}, '${producto.nombre}', ${producto.precio_base}, '${producto.imagen_principal}')">
                             Añadir al carrito
                         </button>
                     </div>
@@ -59,7 +60,45 @@ async function cargarCatalogo() {
     }
 }
 
-// Función simulada para el botón (la programaremos a fondo después)
-function agregarAlCarrito(idProducto) {
-    alert(`Prenda con ID ${idProducto} añadida al carrito. ¡Paso a paso!`);
+// --- Lógica del Carrito de Compras ---
+
+// Inicializar el carrito: busca si ya hay algo guardado, si no, crea un arreglo vacío
+let carrito = JSON.parse(localStorage.getItem('carritoBW')) || [];
+
+// Función para atrapar el producto al hacer clic
+function agregarAlCarrito(idProducto, nombre, precio, imagen) {
+    // Revisar si la prenda ya está en la bolsa
+    const prendaExistente = carrito.find(item => item.id === idProducto);
+    
+    if (prendaExistente) {
+        prendaExistente.cantidad++; // Si ya está, solo aumentamos la cantidad
+    } else {
+        // Si es nueva, la agregamos al arreglo
+        carrito.push({
+            id: idProducto,
+            nombre: nombre,
+            precio: precio,
+            imagen: imagen,
+            cantidad: 1
+        });
+    }
+    
+    // Guardar en la memoria del navegador
+    localStorage.setItem('carritoBW', JSON.stringify(carrito));
+    
+    // Alerta temporal para confirmar
+    alert(`¡${nombre} se añadió a tu bolsa de compras!`);
+    
+    alert(`¡${nombre} se añadió a tu bolsa de compras!`);
+    console.log("Estado actual del carrito:", carrito);
+    actualizarContadorCarrito(); // <-- Agrega esta línea al final
+}
+// Función para actualizar el número visual en el carrito
+function actualizarContadorCarrito() {
+    const contador = document.getElementById('contador-carrito');
+    if (contador) {
+        // Sumar todas las cantidades de las prendas en el arreglo
+        const totalPrendas = carrito.reduce((total, item) => total + item.cantidad, 0);
+        contador.innerText = totalPrendas;
+    }
 }
